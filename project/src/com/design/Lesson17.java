1ca3cbd77d123e150fe796a65aefecdbea935718
caiya <caiya928@aliyun.com>
2019-09-07 11:12:12 +0800

    测试提交

--- project/src/com/design/Lesson17.java
+++ project/src/com/design/Lesson17.java
@@ -2,6 +2,6 @@ package com.design;
 
 public class Lesson17 {
     public static void main(String[] args) {
-        System.out.println("hello world.");
+        System.out.println("hello world.11");
     }
 }
